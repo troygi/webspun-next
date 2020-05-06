73629952a9a149bd22c0dbc90a3c97a023149c96
Troy Gilliland <troygi@mac.com>
2020-05-06 13:48:13 -0400

    Update about.js
    Removed phone number and email.

--- pages/about.js
+++ pages/about.js
@@ -15,11 +15,13 @@ export default function Home() {
 		I started my career in Madrid, Spain designing CD covers and promotion material for record producers, Juan Tarodo and Jorge Alvarez (Mecano &amp; Olé Olé).
 		In 1998 I moved to Chicago where I landed at Classified Ventures (Cars.com &amp; Apartments.com). While in Chicago, I worked at DiamondCluster and HSBC before becoming a designer/developer for the social news app, Flipboard.  
 		</p>
-	
+		
+		{/* 
 		<p>
 		<a href="tel:630-440-9549">630-440-9549</a> <span class="vr">|</span>
 		<a href="mailto:troy@webspun.org">troy@webspun.org</a> 
 		</p>
+		*/}
 
   	</Layout>
   	
