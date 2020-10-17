c36ecd281e6cd4cad59021b0ae2949cab0c448a0
Troy Gilliland <troygi@mac.com>
2020-10-17 07:14:00 -0400

    Add drone rotation and movement

--- pages/playground/drone-vr-raycaster-hook.js
+++ pages/playground/drone-vr-raycaster-hook.js
@@ -17,9 +17,27 @@ import { BoxLineGeometry } from '../../node_modules/three/examples/jsm/geometrie
 export default function Scene() {
 
 	const canvasId = "canvas"
-	var scene3D, cube, drone, controller1
+	var scene3D, cube, drone, moveDrone
 	const [color, setColor] = useState("red")
-	const [speed, setSpeed] = useState(.25);
+	//const [speed, setSpeed] = useState(.15)
+	
+	var speed = .03
+	var propSpeed = .15
+	
+	var angle = 0
+	
+	// 90 var angle = 1.5708
+	// 45 var angle = 0.785398
+	// 180 var angle = 3.14159
+	// 225 var angle = 3.92699
+	// 270 var angle = 4.71239
+	// 315 var angle = 5.49779
+	// 360 var angle = 6.28319
+	
+	var moveDrone = false
+	var rotateDrone = false
+	
+	var controllerModelFactory = new XRControllerModelFactory()
 	
 	useEffect(() => {
 		
@@ -41,55 +59,68 @@ export default function Scene() {
 		drone.group.position.x = 0
 		drone.group.position.y = 1.5
 		drone.group.position.z = -2.5
-		drone.group.rotation.z = .3
-		drone.group.rotation.y = .5
+		drone.group.rotation.y = angle
+		
+		//drone.group.rotateY(THREE.Math.degToRad(50)) //.5
 		
 		
-		// XR Controller
-		controller1 = scene3D.renderer.xr.getController( 0 );
-		controller1.addEventListener( 'connected', function ( event ) {
-			/*
-			this.add( 
-				buildController( event.data ) 
-			);
-			*/
-		} );
-		controller1.addEventListener( 'disconnected', function () {
-
-			this.remove( 
-				this.children[ 0 ] 
-			);
-		} );
+		// XR Controller 1
+		var controller1 = scene3D.renderer.xr.getController( 0 )
+		controller1.addEventListener( 'selectstart', onSelectStartMove )
+		controller1.addEventListener( 'selectend', onSelectEndMove )
+		scene3D.scene.add(controller1)
 		
-		controller1.addEventListener( 'selectstart', onSelectStart );
-		controller1.addEventListener( 'selectend', onSelectEnd );
+		var controllerGrip1 = scene3D.renderer.xr.getControllerGrip( 0 )
+		controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) )
+		scene3D.scene.add( controllerGrip1 )
 		
-		scene3D.scene.add(controller1);
+		
+		// XR Controller 2
+		var controller2 = scene3D.renderer.xr.getController( 1 )
+		controller2.addEventListener( 'selectstart', onSelectStartRotate )
+		controller2.addEventListener( 'selectend', onSelectEndRotate )
+		scene3D.scene.add(controller2)
+		
+		var controllerGrip2 = scene3D.renderer.xr.getControllerGrip( 1 )
+		controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) )
+		scene3D.scene.add( controllerGrip2 )
 		
 		// XR
-		document.getElementById("vr-controls").appendChild( VRButton.createButton( scene3D.renderer ) );
+		document.getElementById("vr-controls").appendChild( VRButton.createButton( scene3D.renderer ) )
 		scene3D.renderer.xr.enabled = true
   	});
   	
   	// XR Controller
   	function buildController( data ) {
   	
-  	
   	}
   	
-  	function onSelectStart() {
+  	function onSelectStartMove() {
 
-		//this.userData.isSelecting = true;
+		cube.material.color.set( "blue" )
 		
-		cube.material.color.set( "blue" );
-
+		propSpeed = .50
+		moveDrone = true
 	}
+	
 
-	function onSelectEnd() {
+	function onSelectEndMove() {
 	
 		cube.material.color.set( "red" );
-		//this.userData.isSelecting = false;
+		
+		propSpeed = .15
+		moveDrone = false
+	}
+	
+	function onSelectStartRotate() {
 
+		rotateDrone = true
+	}
+	
+
+	function onSelectEndRotate() {
+	
+		rotateDrone = false
 	}
   	
   	function createGround(color) {
@@ -97,26 +128,45 @@ export default function Scene() {
 		var color = new THREE.MeshLambertMaterial({color: color})
 		var cube = new THREE.Mesh( geometry, color )
 		
-		scene3D.scene.add(cube)
+		//scene3D.scene.add(cube)
 	}
   	
   	function animate() {
 		
-		//setSpeed(.50)
-		
+		/*
 		cube.rotation.x += 0.01
 		cube.rotation.y += 0.01	
 		
 		cube.position.x = 2
 		cube.position.z = -5
 		cube.position.y = .9
+		*/
 		
 		// Animate props
-		drone.propFL.rotation.y += speed;
-		drone.propFR.rotation.y += speed;
-		drone.propBL.rotation.y += speed;
-		drone.propBR.rotation.y += speed;
+		drone.propFL.rotation.y += propSpeed;
+		drone.propFR.rotation.y += propSpeed;
+		drone.propBL.rotation.y += propSpeed;
+		drone.propBR.rotation.y += propSpeed;
 		
+		if (moveDrone == true) {
+			
+			//drone.group.position.x += 0.01
+			//drone.group.position.z += 0.01
+			
+			drone.group.position.x += speed * Math.sin(angle)
+			drone.group.position.z += speed * Math.cos(angle)
+			
+			
+			//numberX += speed * Math.sin(angle)
+			//numberY -= speed * Math.sin(angle)
+		}
+		
+		if (rotateDrone == true) {
+			
+			angle = drone.group.rotation.y += 0.01
+		}
+		
+	
 		render()
 	}
 	
